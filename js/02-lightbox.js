b8bc8f9b5bbdcb35e3f3b9dfcb5473511f496c3d
PokhilenkoD <pokhilenko.d@gmail.com>
2022-11-01 17:09:44 +0200

    adds solved second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryParentEl = document.querySelector('.gallery');
+
+const murkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li>
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+      </li>`
+  )
+  .join('');
+
+galleryParentEl.insertAdjacentHTML('beforeend', murkup);
+
+galleryParentEl.addEventListener('click', onClickGalleryElements);
+
+function onClickGalleryElements(event) {
+  event.preventDefault();
+}
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
